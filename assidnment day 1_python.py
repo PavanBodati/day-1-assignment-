1b97ad7f3ef28cbfaad6d437197e67f6e47aa6d2
PavanBodati <89032601+PavanBodati@users.noreply.github.com>
2021-08-16 22:10:10 +0530

    day 1 assignment submission

---
+++ assidnment day 1_python.py
@@ -0,0 +1,10 @@
+#problem statement :WAP TO TAKE 2 INPUTS x and y from the user and to print the power of 2 variables 
+
+
+
+
+x=int(input('enter the first value'))
+
+y= int(input('enter the second value'))
+res=x**y
+print(res)
